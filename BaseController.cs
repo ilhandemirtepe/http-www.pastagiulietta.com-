b96da6d2736a0d36c5ac2ccff6c0e348f7a848e2
ilhandemirtepe <ilhandemirtepe@gmail.com>
2017-06-15 13:22:25 +0300

    Add files via upload

---
+++ BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PastaMVC.Utils
+{
+    public class BaseController : System.Web.Mvc.Controller
+    {
+
+        protected override void OnActionExecuting(ActionExecutingContext filtercontext)
+        {
+            if (Session["Adminid"] == null)
+            {
+                filtercontext.Result = new RedirectResult("~/Login");
+                return;
+            }
+            base.OnActionExecuting(filtercontext);
+        }
+    }
+}
